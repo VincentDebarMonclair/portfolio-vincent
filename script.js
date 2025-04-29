5f899ee794c23e9723255a5372361f6d2845835d
VincentDebarMonclair <vince.mdm@gmail.com>
2025-04-29 23:17:40 +0200

    JS added + about page update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".fade-in-element");
 
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  elements.forEach((el) => observer.observe(el));
+});
